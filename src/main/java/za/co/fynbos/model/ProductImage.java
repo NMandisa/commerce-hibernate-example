425ec1e9cad580abf789e0a8596ad9ff6e3ada0f
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2023-11-05 22:09:01 +0200

    removed commented out code

--- src/main/java/za/co/fynbos/model/ProductImage.java
+++ src/main/java/za/co/fynbos/model/ProductImage.java
@@ -11,11 +11,6 @@ import java.io.Serializable;
 @Embeddable
 public class ProductImage implements Serializable {
 
-    /*@Id
-    @SequenceGenerator(name = "product_generator", sequenceName = "product_sequence", allocationSize = 1,initialValue = 10)
-    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_generator")
-    @Column(name = "product_image_id")
-    private Long productImageId;*/
     @Column(name = "product_image_url")
     private String productImageUrl;
 
