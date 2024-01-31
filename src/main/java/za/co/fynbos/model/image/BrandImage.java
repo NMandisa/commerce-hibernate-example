8cd7bf4cb1f55b2920894183a7687183ba2d51b5
Noxolo Mandisa Mkhungo <NMandisa@users.noreply.github.com>
2024-01-31 17:59:16 +0200

    overriding attributes of MappedSuperClass

--- src/main/java/za/co/fynbos/model/image/BrandImage.java
+++ src/main/java/za/co/fynbos/model/image/BrandImage.java
@@ -4,7 +4,6 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import za.co.fynbos.model.Brand;
-import za.co.fynbos.model.Product;
 
 /**
  * @author Noxolo.Mkhungo
@@ -12,6 +11,9 @@ import za.co.fynbos.model.Product;
 @Getter
 @Setter
 @Entity
+@AttributeOverride(name="imageId", column=@Column(name="brand_image_id"))
+@AttributeOverride(name="imageName", column=@Column(name="brand_image_name"))
+@AttributeOverride(name="imageUrl", column=@Column(name="brand_image_url"))
 @Table(name = "brand_image",schema = "db_commerce")
 @DiscriminatorValue("brand_images")
 public class BrandImage extends AbstractImage{
@@ -19,7 +21,7 @@ public class BrandImage extends AbstractImage{
     @ManyToOne(cascade = CascadeType.ALL,fetch = FetchType.LAZY)
     @JoinTable(
             name = "brand_has_brand_images",
-            joinColumns = @JoinColumn(name = "brand_image_id", referencedColumnName = "image_id"),
+            joinColumns = @JoinColumn(name = "brand_image_id", referencedColumnName = "brand_image_id"),
             inverseJoinColumns = @JoinColumn(name = "brand_id", referencedColumnName = "brand_id",foreignKey=@ForeignKey(name = "brand_image_id_fk")
             ))
     private Brand brand;
--- src/main/java/za/co/fynbos/model/image/CategoryImage.java
+++ src/main/java/za/co/fynbos/model/image/CategoryImage.java
@@ -3,7 +3,6 @@ package za.co.fynbos.model.image;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
-import za.co.fynbos.model.Brand;
 import za.co.fynbos.model.Category;
 
 /**
@@ -12,6 +11,9 @@ import za.co.fynbos.model.Category;
 @Getter
 @Setter
 @Entity
+@AttributeOverride(name="imageId", column=@Column(name="category_image_id"))
+@AttributeOverride(name="imageName", column=@Column(name="category_image_name"))
+@AttributeOverride(name="imageUrl", column=@Column(name="category_image_url"))
 @Table(name = "category_image",schema = "db_commerce")
 @DiscriminatorValue("category_images")
 public class CategoryImage extends AbstractImage{
@@ -19,7 +21,7 @@ public class CategoryImage extends AbstractImage{
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinTable(
             name = "category_has_category_images",
-            joinColumns = @JoinColumn(name = "category_image_id", referencedColumnName = "image_id"),
+            joinColumns = @JoinColumn(name = "category_image_id", referencedColumnName = "category_image_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id", referencedColumnName = "category_id",foreignKey=@ForeignKey(name = "category_id_fk")
             ))
     private Category category;
